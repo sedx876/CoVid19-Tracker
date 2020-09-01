414223e9915bdc8e360e9b660306244eefcc8f12
sedx876 <swatkins876@gmail.com>
2020-09-01 10:15:23 -0400

    dropdown menu material-ui dependency added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import React from 'react';
 import './styles/App.css';
+import { FormControl, Select, MenuItem } from '@material-ui/core'
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+    {/*Header */}
+    <div className="app__header">
+      <h1>&#10009;CoVid19 Tracker&#10009;</h1>
+
+    {/*Title + Select Dropdown Field */}
+      <FormControl className="app__dropdown">
+        <Select variant="outlined" value="abc">
+          <MenuItem value="worldwide">WorldWide</MenuItem>
+          <MenuItem value="worldwide">Option 1</MenuItem>
+          <MenuItem value="worldwide">Option 2</MenuItem>
+          <MenuItem value="worldwide">Option 3</MenuItem>
+        </Select>
+      </FormControl>
+      </div>
       
+
+      
+
+      {/*InfoBox*/}
+
+      {/*InfoBox*/}
+
+      {/*InfoBox*/}
+
+      {/*Table*/}
+
+      {/*Graph*/}
+
+      {/*Map*/}
     </div>
   );
 }
